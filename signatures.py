5559726406c70d28b38b33e1bce8f6c184552126
dsadep <polakovk421@gmail.com>
2025-02-11 00:27:14 +0300

    signatures generator changed

--- signatures.py
+++ signatures.py
@@ -1,26 +1,18 @@
 import hashlib
 
 def generate_signature(bill_id: int, amount: float, transaction_id: int, user_id: int, secret_key: str) -> str:
-    """
-    Генерирует сигнатуру для вебхука на основе входных данных.
-
-    :param account_id: Идентификатор аккаунта
-    :param amount: Сумма транзакции
-    :param transaction_id: Идентификатор транзакции
-    :param user_id: Идентификатор пользователя
-    :param secret_key: Секретный ключ для генерации сигнатуры
-    :return: Сгенерированная сигнатура в виде строки
-    """
     signature_string = f"{bill_id}{amount}{transaction_id}{user_id}{secret_key}"
     return hashlib.sha256(signature_string.encode()).hexdigest()
 
-# Пример использования
+"""
+Helping file to generate signature for the payment gateway.
+"""
 
-secret_key = "BHwQl0LeyoEXGnIyYpBF5Nj5EUewpBfm8ynfJGn6hOk="
-bill_id = 7
-amount = 100.0
-transaction_id = "cd0-472c-bd36-35660f00132b"
-user_id = 4
+secret_key = "your_secret_key"
+bill_id = 0
+amount = 0.0
+transaction_id = "transaction_id"
+user_id = 0
 
 signature = generate_signature(bill_id, amount, transaction_id, user_id, secret_key)
 print("Generated Signature:", signature)
